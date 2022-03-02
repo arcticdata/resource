59c3ef70bd0d5d2de44c03f25cd882e7e3e9bd6b
huichengcheng <huichengcheng@datarc.cn>
2022-03-02 12:34:36 +0800

    add Deepin

--- resource/Deepin/nginx/nginx_install.sh
+++ resource/Deepin/nginx/nginx_install.sh
@@ -7,5 +7,5 @@ EOF
 wget https://r.datarc.cn/Deepin/nginx/nginx_signing.key && apt-key add nginx_signing.key
 ####### 解决依赖
 wget https://r.datarc.cn/Deepin/nginx/libssl1.0.0_1.0.2n-1ubuntu5_amd64.deb && dpkg -i libssl1.0.0_1.0.2n-1ubuntu5_amd64.deb
-####### 安装 nginx
+####### 安装 Nginx
 sudo apt update && sudo apt install nginx -y
